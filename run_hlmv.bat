afbf712b92a6af4f45d4b4d3ac28a2af2abdf25a
aaronweiss74 <aaronweiss74@a6d7ee8f-d40e-d1f1-f2a9-9200d20d501c>
2010-05-19 03:34:18 +0000

    Committing full, unmodified project fresh out of Source SDK

---
+++ run_hlmv.bat
@@ -0,0 +1,2 @@
+@"c:\program files\steam\steamapps\elvenspike\sourcesdk\bin\hlmv.exe" -game "c:\program files\steam\steamapps\SourceMods\puzzlr" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
---
+++ run_mod.bat
@@ -0,0 +1,1 @@
+@"c:\program files\steam\steamapps\elvenspike\source sdk base\hl2.exe" -dev -game "c:\program files\steam\steamapps\SourceMods\puzzlr" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
